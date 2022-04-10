de12c1ecee7e8c5b1ce55f6adcc5ee2753458635
Bernardo <bebezanetti@gmail.com>
2022-04-10 14:45:10 -0300

    main

--- numero maior entre 3/Maior.c
+++ numero maior entre 3/Maior.c
@@ -14,23 +14,23 @@ int main(){
     if(a>b){
         if(a>c){
           if(b>c){
-              printf("\nOs numeros em ordem sao:%d, %d, %d", a, b, c);
+              printf("\nOs numeros em ordem crescente sao: %d, %d, %d", a, b, c);
           } else { //Else do if da linha 16
-                printf("\nOs numeros em ordem sao:%d, %d, %d", a, c, b);
+                printf("\nOs numeros em ordem crescente sao: %d, %d, %d", a, c, b);
           }// Fim do else da linha 18
         } else { //Else do if da linha 15 para se a<c
-            printf("\nOs numeros em ordem sao:%d, %d, %d", c, a, b);
+            printf("\nOs numeros em ordem crescente sao: %d, %d, %d", c, a, b);
         } //Fim do else da linha 21 
     } else { //Else do if da linha 14 para se a<b
         if(b>c){
             if(a>c){
-                printf("\nOs numeros em ordem sao:%d, %d, %d", b, a, c);
+                printf("\nOs numeros em ordem crescente sao: %d, %d, %d", b, a, c);
             } else { // Else do if da linha 26
-                printf("\nOs numeros em ordem sao:%d, %d, %d", b, c, a);
+                printf("\nOs numeros em ordem crescente sao: %d, %d, %d", b, c, a);
             }
         } else { //Else do if da linha 25
             if(b>a){
-                printf("\nOs numeros em ordem sao:%d, %d, %d", c, b, a);
+                printf("\nOs numeros em ordem crescente sao: %d, %d, %d", c, b, a);
             }//Fim do if da linha 32
         }//Fim do else da linha 31
     }//Fim do else da linha 24
