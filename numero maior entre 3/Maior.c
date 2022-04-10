7a86dbcf9f1e894c134c20bb4ad47da2d626c8d7
Bernardo <bebezanetti@gmail.com>
2022-04-10 14:38:55 -0300

    main

--- numero maior entre 3/Maior.c
+++ numero maior entre 3/Maior.c
@@ -5,31 +5,33 @@ int main(){
     int a, b, c; //declaração de variaveis
 
     printf("Escreva o primeiro numero: \n");
-    scanf("%d", &a);
+    scanf("%d", &a);//Leitura do primeiro numero para variavel "a"
     printf("Escreva o segundo numero:\n");
-    scanf("%d", &b);
+    scanf("%d", &b);//Leitura do segundo numero para variavel "b"
     printf("Escreva o terceiro numero:\n");
-    scanf("%d", &c);
+    scanf("%d", &c);//Leitura do terceiro numero para variavel "c"
     
     if(a>b){
         if(a>c){
           if(b>c){
               printf("\nOs numeros em ordem sao:%d, %d, %d", a, b, c);
-        } else {
+          } else { //Else do if da linha 16
                 printf("\nOs numeros em ordem sao:%d, %d, %d", a, c, b);
-            }
+          }// Fim do else da linha 18
         } else { //Else do if da linha 15 para se a<c
             printf("\nOs numeros em ordem sao:%d, %d, %d", c, a, b);
-        }  
+        } //Fim do else da linha 21 
     } else { //Else do if da linha 14 para se a<b
         if(b>c){
             if(a>c){
                 printf("\nOs numeros em ordem sao:%d, %d, %d", b, a, c);
-            } else{
+            } else { // Else do if da linha 26
                 printf("\nOs numeros em ordem sao:%d, %d, %d", b, c, a);
             }
-        } else{
-            ("\nOprintfs numeros em ordem sao:%d, %d, %d", c, b, a);
-        }
-    }
-}
+        } else { //Else do if da linha 25
+            if(b>a){
+                printf("\nOs numeros em ordem sao:%d, %d, %d", c, b, a);
+            }//Fim do if da linha 32
+        }//Fim do else da linha 31
+    }//Fim do else da linha 24
+}//Fim do código
