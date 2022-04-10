7e136c5c1691373824353b7f2d01adb93b7e9c35
Bernardo <bebezanetti@gmail.com>
2022-04-10 12:57:43 -0300

    main

--- numero maior entre 3/Maior.c
+++ numero maior entre 3/Maior.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(){
-    int a, b, c;
+    int a, b, c; //declaração de variaveis
 
     printf("Escreva o primeiro numero: \n");
     scanf("%d", &a);
@@ -15,7 +15,21 @@ int main(){
         if(a>c){
           if(b>c){
               printf("\nOs numeros em ordem sao:%d, %d, %d", a, b, c);
-          }  
+        } else {
+                printf("\nOs numeros em ordem sao:%d, %d, %d", a, c, b);
+            }
+        } else { //Else do if da linha 15 para se a<c
+            printf("\nOs numeros em ordem sao:%d, %d, %d", c, a, b);
+        }  
+    } else { //Else do if da linha 14 para se a<b
+        if(b>c){
+            if(a>c){
+                printf("\nOs numeros em ordem sao:%d, %d, %d", b, a, c);
+            } else{
+                printf("\nOs numeros em ordem sao:%d, %d, %d", b, c, a);
+            }
+        } else{
+            ("\nOprintfs numeros em ordem sao:%d, %d, %d", c, b, a);
         }
     }
 }
