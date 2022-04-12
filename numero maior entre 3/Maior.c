0fbbfc5d363939ef3e97f3bb3ad2e9ccce34c38d
Bernardo <bebezanetti@gmail.com>
2022-04-12 15:14:47 -0300

    Mudança de comentarios

--- numero maior entre 3/Maior.c
+++ numero maior entre 3/Maior.c
@@ -17,27 +17,27 @@ int main(){
         if(a>c){
           if(b>c){
               printf("\nO numero %d e maior que o numero %d e o numero %d.\n", a, b, c);
-          } else { //Else do if da linha 16
+          } else { //Else do if da linha 118
                 printf("\nO numero %d e maior que o numero %d e o numero %d.\n", a, c, b);
-          }// Fim do else da linha 18
-        } else { //Else do if da linha 15 para se a<c
+          }// Fim do else da linha 20
+        } else { //Else do if da linha 17 para se a<c
             printf("\nO numero %d e maior que o numero %d e o numero %d.\n", c, a, b);
-        } //Fim do else da linha 21 
-    } else { //Else do if da linha 14 para se a<b
+        } //Fim do else da linha 23
+    } else { //Else do if da linha 16 para se a<b
         if(b>c){
             if(a>c){
                 printf("\nO numero %d e maior que o numero %d e o numero %d.\n", b, a, c);
-            } else { // Else do if da linha 26
+            } else { // Else do if da linha 28
                 printf("\nO numero %d e maior que o numero %d e o numero %d.\n", b, c, a);
-            }//Fim do Else da linha 28
-        } else { //Else do if da linha 25
+            }//Fim do Else da linha 30
+        } else { //Else do if da linha 27
             if(b>a){
                 printf("\nO numero %d e maior que o numero %d e o numero %d.\n", c, b, a);
-            }//Fim do if da linha 32
-        }//Fim do else da linha 31
-    }//Fim do else da linha 24
+            }//Fim do if da linha 34
+        }//Fim do else da linha 33
+    }//Fim do else da linha 26
     if(a==b && b==c){ //Caso todos os numeros sejam iguais
         printf("\nTodos os numeros sao iguais: %d, %d, %d.\n", a, b, c);
-    }//Fim do if da linha 37
+    }//Fim do if da linha 39
     system("PAUSE");//Para o executável não fechar sozinho
 }//Fim do código
