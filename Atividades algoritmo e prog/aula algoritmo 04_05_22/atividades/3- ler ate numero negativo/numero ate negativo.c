9172a0de08f2789bad969480c4f6e24f8f6764bc
Bernardo <bebezanetti@gmail.com>
2022-05-05 15:54:50 -0300

    main

--- Atividades algoritmo e prog/aula algoritmo 04_05_22/atividades/3- ler ate numero negativo/numero ate negativo.c
+++ Atividades algoritmo e prog/aula algoritmo 04_05_22/atividades/3- ler ate numero negativo/numero ate negativo.c
@@ -9,3 +9,49 @@ menor valor
 */
 
 
+#include <stdio.h>
+
+int main(){
+    int maior, menor, aux, pares, impares, zeros, media, cont;
+    maior = 0;
+    zeros = 0;
+    pares = 0;
+    impares = 0;
+    media = 0;
+    cont = 0;
+    menor = 99999;
+    
+    do{
+        printf("\n informe um numero: ");
+        scanf("%d", &aux);
+        if (aux==0)zeros++;
+        if (aux>0){
+            if (aux%2==0){
+                pares++;   
+            }else{
+                impares++;
+            }
+            if (aux>maior)maior=aux;  
+            if (aux>0 && aux<menor)menor=aux;
+            media= media+aux;  
+            cont++;
+        }   
+    }while(aux>=0);
+
+    
+    
+    media = media/cont;
+   
+   
+    printf("\nO total de numeros lidos foi: %d", cont);
+    printf("\nO total de pares lidos foi: %d", pares);
+    printf("\nO total de impares lidos foi: %d", impares);
+    printf("\nO total de zeros lidos foi: %d", zeros);
+    printf("\nA media total foi: %d", media);
+    printf("\nO maior valor foi: %d", maior);
+    printf("\nO menor valor foi: %d\n", menor);
+
+    system("pause");
+
+
+}
--- Atividades algoritmo e prog/aula algoritmo 04_05_22/atividades/4- advinhe o numero/adivinhe.c
+++ Atividades algoritmo e prog/aula algoritmo 04_05_22/atividades/4- advinhe o numero/adivinhe.c
@@ -8,7 +8,36 @@ o programa deve gerar um numero randomico de 1 a 100 no inicio e o usuario tem 6
     senao: game over ! perdeu!
 */
 #include <stdio.h>
-int main (){
+#include <stdlib.h>
+#include<time.h>
 
+int main (){
+    int aleatorio, numero, tentativas, cont;
+    srand(time(NULL));
+    aleatorio =1+ rand()%100;
     
+    tentativas = 6;
+    cont=0;
+    printf("Jogo adivinhe o numero.");
+    do {
+        if (cont!=6){
+             printf("\nEscolha um numero (%d tentativas restantes): ", tentativas);
+             scanf("%d",&numero);
+            if (numero>aleatorio)printf("O numero e menor.");
+            if (numero<aleatorio)printf("O numero e maior.");
+            cont++;
+            tentativas--;
+        }else{
+            numero = aleatorio;
+            cont++;
+        }
+    }while(numero!=aleatorio);
+
+    if (cont<=3)printf("\nAcertou e e muito sortudo.\n");
+    if (cont<=5 && cont>3)printf("\nAcertou.\n");
+    if (cont==6)printf("\nAcertou e e muito sortudo.\n");
+    if (cont==7)printf("\nGame over! perdeu, o numero era: %d\n",aleatorio);
+
+
+    system("pause");
 }
