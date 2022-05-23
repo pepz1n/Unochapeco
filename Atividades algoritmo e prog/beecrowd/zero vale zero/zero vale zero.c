bd3fd43da7662773a2866d2f11927257f80f3214
Bernardo <bebezanetti@gmail.com>
2022-05-23 19:21:24 -0300

    Update zero vale zero.c

--- Atividades algoritmo e prog/beecrowd/zero vale zero/zero vale zero.c
+++ Atividades algoritmo e prog/beecrowd/zero vale zero/zero vale zero.c
@@ -5,18 +5,20 @@
 int main(){
     int numero1, numero2, digitos;
     char num[30];
-    scanf("%d %d",&numero1,&numero2);
 
-    numero1 += numero2;
-    itoa(numero1, num, 10);
-    digitos = strlen(num);
+    do{
+        scanf("%d %d",&numero1,&numero2);
+        numero1 += numero2;
+        itoa(numero1, num, 10);
+        digitos = strlen(num);
 
-    for(int i=0; i<digitos; i++){
-        if(num[i] != '0'){
-            printf("%c", num[i]);
+        for(int i=0; i<digitos; i++){
+            if(num[i] != '0'){
+                printf("%c", num[i]);
+            }
         }
-    }
-    
-    system ("pause");
-
+        printf("\n");
+    }while(numero1!=0 &&numero2!=0);
+    printf("\n");
+    return 0;
 }
