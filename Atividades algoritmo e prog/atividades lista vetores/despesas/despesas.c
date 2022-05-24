027d47f5f81cc4f5b8a7e2a2f08047872774bce5
Bernardo <bebezanetti@gmail.com>
2022-05-24 15:40:08 -0300

    main

--- Atividades algoritmo e prog/atividades lista vetores/despesas/despesas.c
+++ Atividades algoritmo e prog/atividades lista vetores/despesas/despesas.c
@@ -11,3 +11,30 @@ DESPESAS:   R$ 2.100,00
 SALDO:      R$ 200,00
 
 */
+
+#include <stdio.h>
+
+int main(){
+    float despesas[5];
+    float salario;
+    float totaldespesas = 0;
+    float saldo;
+
+    for(int i=0; i<5;i++){
+        printf("\nDigite o valor da despesa: ");
+        scanf("%f",&despesas[i]);
+        totaldespesas+=despesas[i];
+    }
+    printf("\nDigite o seu salario: ");
+    scanf("%f",&salario);
+    saldo = salario-totaldespesas;
+    
+    printf("\nRELATORIO MENSAL:");
+    printf("\nSuas despesas:");
+    for(int i=0; i<5; i++)printf(" %.2f",despesas[i]);
+    printf("\nSALARIO:    R$ %.2f",salario);
+    printf("\nDESPESAS:   R$ %.2f",totaldespesas);
+    printf("\n----------------------");
+    printf("\nSALDO:      R$ %.2f",saldo);
+    
+}
